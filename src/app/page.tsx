c5f7675a19e89c1ce7cf9b3b28a7afee91331bd2
Software Clickmasters <software.clickmasters@gmail.com>
2026-06-24 11:19:34 +0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,17 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://clickmastersdigitalmarketing.com/",
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-snippet": -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+    },
+  },  
 };
 
 export default function Home() {
